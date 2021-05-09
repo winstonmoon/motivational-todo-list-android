02d8c9ca54810f5d260cca1c1808787136861baf
Sungjun Moon <project_m_@hotmail.com>
2021-05-09 16:57:59 +0900

    delete actiion bar

--- app/src/main/java/com/moonwinston/motivationaltodolist/MainActivity.kt
+++ app/src/main/java/com/moonwinston/motivationaltodolist/MainActivity.kt
@@ -23,9 +23,9 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.fragment_nav_host)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_daily, R.id.navigation_monthly, R.id.navigation_weekly, R.id.navigation_reward, R.id.navigation_add))
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.navigation_daily, R.id.navigation_monthly, R.id.navigation_weekly, R.id.navigation_reward, R.id.navigation_add))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         binding.viewNav.setupWithNavController(navController)
 
         binding.buttonSettings.setOnClickListener {
