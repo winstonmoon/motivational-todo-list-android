4f028bac43f8d58f66eb1597ecfd5b017a6f7337
Sungjun Moon <project_m_@hotmail.com>
2021-06-20 18:11:13 +0900

    add room prototype

--- app/src/main/java/com/moonwinston/motivationaltodolist/data/Task.kt
+++ app/src/main/java/com/moonwinston/motivationaltodolist/data/Task.kt
@@ -1,15 +1,23 @@
 package com.moonwinston.motivationaltodolist.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.sql.Time
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.*
 
+@Entity(tableName = "task")
 data class Task(
-//        var month: Int,
-//        var dayOfMonth: Int,
-//        var dayOfWeek: Int,
-        //val time: Time,
-        var time: String,
-        var task: String,
-        var isGoalSet: Boolean,
-//        var isCompleted: Boolean
+//        val time: String,
+//        var task: String,
+//        var isGoalSet: Boolean
+//){constructor():this("12:00","test",true)}
+        @PrimaryKey(autoGenerate = true) val uid: Int,
+        @ColumnInfo(name = "taskDate")var taskDate: Date,
+        @ColumnInfo(name = "taskTime")var taskTime: Date,
+        @ColumnInfo(name = "task")var task: String,
+        @ColumnInfo(name = "isGoalSet")var isGoalSet: Boolean,
+        @ColumnInfo(name = "isCompleted") var isCompleted: Boolean
 )
