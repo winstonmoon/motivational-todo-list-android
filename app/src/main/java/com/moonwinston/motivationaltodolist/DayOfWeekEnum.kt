81176d3005fe21f436b24e3b3092547349bfd350
Sungjun Moon <project_m_@hotmail.com>
2021-09-08 23:54:29 +0900

    fix weekly fragment in progress

--- app/src/main/java/com/moonwinston/motivationaltodolist/DayOfWeekEnum.kt
+++ app/src/main/java/com/moonwinston/motivationaltodolist/DayOfWeekEnum.kt
@@ -6,11 +6,11 @@ enum class DayOfWeekEnum(@StringRes val dayNumberOfWeek: Int,
                          @StringRes val dayOfWeek: Int
 ) {
     None(R.string.none_number,R.string.none_string),
-    Monday(R.string.mon_number, R.string.jan_abbreviation),
-    Tuesday(R.string.tue_number, R.string.feb_abbreviation),
-    Wednesday(R.string.wed_number, R.string.mar_abbreviation),
-    Thursday(R.string.thu_number, R.string.apr_abbreviation),
-    Friday(R.string.fri_number, R.string.may_abbreviation),
-    Saturday(R.string.sat_number, R.string.jun_abbreviation),
-    Sunday(R.string.sun_number, R.string.jul_abbreviation)
+    Sunday(R.string.sun_number, R.string.sun_string),
+    Monday(R.string.mon_number, R.string.mon_string),
+    Tuesday(R.string.tue_number, R.string.tue_string),
+    Wednesday(R.string.wed_number, R.string.wed_string),
+    Thursday(R.string.thu_number, R.string.thu_string),
+    Friday(R.string.fri_number, R.string.fri_string),
+    Saturday(R.string.sat_number, R.string.sat_string)
 }
