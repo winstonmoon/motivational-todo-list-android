6c49bb1aeb6b2a55f6e596abc5041b41998aa31b
Sungjun Moon <project_m_@hotmail.com>
2021-10-23 01:41:45 +0900

    change select language from BaseActivity to ContextUtil

--- app/src/main/java/com/moonwinston/motivationaltodolist/utils/ContextUtil.kt
+++ app/src/main/java/com/moonwinston/motivationaltodolist/utils/ContextUtil.kt
@@ -8,14 +8,30 @@ import android.os.Build
 import android.os.LocaleList
 import java.util.*
 
-class ContextUtil (base: Context) : ContextWrapper(base) {
+class ContextUtil(base: Context) : ContextWrapper(base) {
 
     companion object {
 
-        fun updateLocale(c: Context, localeToSwitchTo: Locale): ContextWrapper {
+        fun updateLocale(c: Context, language: Int): ContextWrapper {
             var context = c
             val resources: Resources = context.resources
             val configuration: Configuration = resources.configuration
+
+            val localeToSwitchTo = when (language) {
+                1 -> Locale.US
+                2 -> Locale.KOREA
+                3 -> Locale.JAPAN
+                4 -> Locale.SIMPLIFIED_CHINESE
+                else -> {
+                    var deviceLanguage =
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) Resources.getSystem().configuration.locales[0]
+                        else Resources.getSystem().configuration.locale
+                    val availableLanguageList =
+                        listOf(Locale.US, Locale.KOREA, Locale.JAPAN, Locale.SIMPLIFIED_CHINESE)
+                    if (availableLanguageList.contains(deviceLanguage)) deviceLanguage else Locale.ENGLISH
+                }
+            }
+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                 val localeList = LocaleList(localeToSwitchTo)
                 LocaleList.setDefault(localeList)
