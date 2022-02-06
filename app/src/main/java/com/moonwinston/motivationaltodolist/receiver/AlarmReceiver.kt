e4f4669c0cabc5f1d7e66b8afedc360266e4f99e
Sungjun Moon <project_m_@hotmail.com>
2022-02-06 16:42:08 +0900

    fix #197 in prgoress

--- app/src/main/java/com/moonwinston/motivationaltodolist/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/moonwinston/motivationaltodolist/receiver/AlarmReceiver.kt
@@ -1,23 +1,41 @@
 package com.moonwinston.motivationaltodolist.receiver
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.moonwinston.motivationaltodolist.MainActivity
 
 class AlarmReceiver : BroadcastReceiver() {
 
-    lateinit var notificationManager: NotificationManager
+    private lateinit var notificationManager: NotificationManager
 
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent) {
         notificationManager = context.getSystemService(
             Context.NOTIFICATION_SERVICE) as NotificationManager
 
         createNotificationChannel()
         deliverNotification(context)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                "Stand up notification",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "AlarmManager Tests"
+            notificationManager.createNotificationChannel(
+                notificationChannel)
+        }
     }
 
     private fun deliverNotification(context: Context) {
@@ -39,21 +57,5 @@ class AlarmReceiver : BroadcastReceiver() {
                 .setDefaults(NotificationCompat.DEFAULT_ALL)
 
         notificationManager.notify(NOTIFICATION_ID, builder.build())
-    }
-
-    fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                PRIMARY_CHANNEL_ID,
-                "Stand up notification",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            notificationChannel.enableLights(true)
-            notificationChannel.lightColor = Color.RED
-            notificationChannel.enableVibration(true)
-            notificationChannel.description = "AlarmManager Tests"
-            notificationManager.createNotificationChannel(
-                notificationChannel)
-        }
     }
 }
